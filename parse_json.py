845e0e01c1c95993fc5501464a20ff06bbd8deb4
Dani Ezzeddine <dani.izzedin@gmail.com>
2018-12-31 02:42:04 -0800

    Final Changes

--- parse_json.py
+++
@@ -1,8 +0,0 @@
-import json
-def parse_json():
-    with open('students.json', 'r') as f:
-        students = json.load(f)
-    students_login = []
-    for student in students:
-        students_login.append(student['login'])
-    return students_login
